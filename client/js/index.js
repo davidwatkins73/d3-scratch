63e10e5a0182f92648a23d4b5df75064282ba98b
David Watkins <davidwatkins73@gmail.com>
2020-10-15 20:27:28 +0100

    roadmap viz
    - added exit handling
    - extracted viz code to new file

--- client/js/index.js
+++ client/js/index.js
@@ -1,89 +1,9 @@
 import '../styles/index.scss';
-import {select} from "d3-selection";
 import {categories, data} from "./data";
-import {drawAxes, mkArcData, mkNodeData, mkScales} from "./utils";
+import {draw} from "./viz";
 
+const redrawFn = draw("#viz", data, categories);
 
-// viz
-
-function drawApps(scales, elem, nodeData = []) {
-    const apps = elem
-        .selectAll("circle.app")
-        .data(nodeData, d => d.milestone.id);
-
-    const newApps = apps
-        .enter()
-        .append("circle")
-        .classed("app", true)
-        .attr("fill", d => scales.color(d.app.id))
-        .attr("stroke", d => scales.color(d.app.id))
-        .on("mouseover", d => console.log(d.milestone.category.name, d.milestone.date))
-
-    apps.merge(newApps)
-        .attr("r", d => d.app.size)
-        .attr("cx", d => scales.x(d.milestone.date))
-        .attr("cy", d => scales.y(d.milestone.category.id) + scales.y.bandwidth() / 2)
-}
-
-
-function drawArcs(scales, elem, arcData = []) {
-    const arcs = elem
-        .selectAll("line.arc")
-        .data(arcData, d => `${d.m1.id}_${d.m2.id}`);
-
-    const newArcs = arcs
-        .enter()
-        .append("line")
-        .classed("arc", true);
-
-    arcs.merge(newArcs)
-        .attr("x1", d => scales.x(d.m1.date))
-        .attr("x2", d => scales.x(d.m2.date))
-        .attr("y1", d => scales.y(d.m1.category.id) + scales.y.bandwidth() / 2)
-        .attr("y2", d => scales.y(d.m2.category.id) + scales.y.bandwidth() / 2);
-}
-
-
-function setupContainers() {
-    const svg = select("#viz")
-        .append("svg")
-        .attr("width", 900)
-        .attr("height", 600);
-
-    const graph = svg
-        .append("g")
-        .attr("transform", "translate(80, 30)");
-
-    const arcs = graph
-        .append("g")
-        .classed("arcs", true);
-
-    const apps = graph
-        .append("g")
-        .classed("apps", true);
-
-    return {
-        svg,
-        arcs,
-        apps
-    };
-}
-
-function draw(data = [],
-              categories = []) {
-
-    const nodeData = mkNodeData(data);
-    const arcData = mkArcData(data);
-    console.log({nodeData, arcData, data, categories})
-
-
-    const scales = mkScales(nodeData, categories);
-    const containers = setupContainers();
-
-
-    drawAxes(scales, containers.svg, categories);
-    drawApps(scales, containers.apps, nodeData);
-    drawArcs(scales, containers.arcs, arcData);
-}
-
-draw(data, categories);
+document.getElementById("y2022").onclick = () => redrawFn(_.filter(data, d => d.app.size > 5));
+document.getElementById("y2024").onclick = () => redrawFn(_.filter(data, d => d.app.size > 10));
+document.getElementById("y2020").onclick = () => redrawFn(data);
---
+++ client/js/viz.js
@@ -0,0 +1,100 @@
+import {select} from "d3-selection";
+import {drawAxes, mkArcData, mkNodeData, mkScales} from "./utils";
+
+// viz
+
+function drawApps(scales, elem, nodeData = []) {
+    const apps = elem
+        .selectAll("circle.app")
+        .data(nodeData, d => d.milestone.id);
+
+    const newApps = apps
+        .enter()
+        .append("circle")
+        .classed("app", true)
+        .attr("fill", d => scales.color(d.app.id))
+        .attr("stroke", d => scales.color(d.app.id))
+        .on("mouseover", d => console.log(d.milestone.category.name, d.milestone.date))
+
+    apps.merge(newApps)
+        .attr("r", d => d.app.size)
+        .attr("cx", d => scales.x(d.milestone.date))
+        .attr("cy", d => scales.y(d.milestone.category.id) + scales.y.bandwidth() / 2)
+
+    apps.exit().remove();
+}
+
+
+function drawArcs(scales, elem, arcData = []) {
+    const arcs = elem
+        .selectAll("line.arc")
+        .data(arcData, d => `${d.m1.id}_${d.m2.id}`);
+
+    const newArcs = arcs
+        .enter()
+        .append("line")
+        .classed("arc", true);
+
+    arcs.merge(newArcs)
+        .attr("x1", d => scales.x(d.m1.date))
+        .attr("x2", d => scales.x(d.m2.date))
+        .attr("y1", d => scales.y(d.m1.category.id) + scales.y.bandwidth() / 2)
+        .attr("y2", d => scales.y(d.m2.category.id) + scales.y.bandwidth() / 2);
+
+    arcs.exit().remove();
+}
+
+
+function setupContainers(elemSelector = '#viz') {
+    const svg = select(elemSelector)
+        .append("svg")
+        .attr("width", 900)
+        .attr("height", 600);
+
+    const graph = svg
+        .append("g")
+        .attr("transform", "translate(80, 30)");
+
+    const arcs = graph
+        .append("g")
+        .classed("arcs", true);
+
+    const apps = graph
+        .append("g")
+        .classed("apps", true);
+
+    return {
+        svg,
+        arcs,
+        apps
+    };
+}
+
+
+function prepareData(rawData, categories) {
+    const nodeData = mkNodeData(rawData);
+    const arcData = mkArcData(rawData);
+    return {nodeData, arcData, categories};
+}
+
+
+export function draw(elemSelector,
+                      rawData = [],
+                      categories = []) {
+
+    const data = prepareData(rawData, categories);
+
+    const scales = mkScales(data.nodeData, data.categories);
+    const containers = setupContainers(elemSelector);
+
+    drawAxes(scales, containers.svg, data.categories);
+
+    drawApps(scales, containers.apps, data.nodeData);
+    drawArcs(scales, containers.arcs, data.arcData);
+
+    return (updatedRawData) => {
+        const updatedData = prepareData(updatedRawData, categories);
+        drawApps(scales, containers.apps, updatedData.nodeData);
+        drawArcs(scales, containers.arcs, updatedData.arcData);
+    };
+}
